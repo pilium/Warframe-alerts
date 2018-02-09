dd7af2d4e79cff8b6c94fbfcabda8348ce093627
Igor Myasnikov <igorpilium@gmail.com>
2018-02-09 20:13:56 +0300

    1

--- src/js/helpers/sortFunc.js
+++ src/js/helpers/sortFunc.js
@@ -1,5 +1,5 @@
 export function compareTierNum(a, b) {
-    if(a.level == b.level) {
+    if(a.tierNum == b.tierNum) {
         if(a.tierNum == b.tierNum) return 0;
         else if(a.tierNum < b.tierNum) return -1;
         else return 1;
